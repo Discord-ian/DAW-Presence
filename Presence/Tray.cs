043061f68db994606cbd77d1fc540d62fc25faf5
Discord-ian <aidanbergeman@yahoo.com>
2020-06-08 01:06:13 -0500

    finished main menu, still need submenu for DAW selection

--- Presence/Tray.cs
+++ Presence/Tray.cs
@@ -10,8 +10,7 @@ namespace DAW_Presence
     {
         private System.Windows.Forms.ContextMenuStrip ctx;
         private System.Windows.Forms.NotifyIcon icon;
-        private System.Windows.Forms.ToolStripMenuItem blacklist;
-        //private MenuItem whitelist;
+        private System.Windows.Forms.ToolStripMenuItem whitelist, blacklist, rpc;
         public Tray()
         {
             this.ShowInTaskbar = false;
@@ -19,18 +18,30 @@ namespace DAW_Presence
             this.ctx = new System.Windows.Forms.ContextMenuStrip();
             this.icon = new NotifyIcon(new Container());
             this.icon.ContextMenuStrip = ctx;
-            this.icon.ContextMenu = new ContextMenu();
-            this.icon.ContextMenuStrip.Items.Add("Add To Blacklist", null, this.ToggleBlacklist);
-            //blacklist = new ToolStripMenuItem();
-            //CheckBlacklist();
-            //this.icon.ContextMenuStrip.Items.Add(blacklist);
-            this.icon.ContextMenuStrip.Items.Add("Add To Whitelist", null, this.ToggleBlacklist);
-            this.icon.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit);
+            InitializeMenu();
             icon.Visible = true;
             icon.Icon = new Icon("ableton.ico");
             Application.Run();
         }
 
+        void InitializeMenu()
+        {
+            rpc = new ToolStripMenuItem();
+            CheckRPCState();
+            this.icon.ContextMenuStrip.Items.Add(rpc);
+            this.icon.ContextMenuStrip.Items.Add("-"); // spacer
+            this.icon.ContextMenuStrip.Items.Add("Add To Blacklist", null, this.ToggleBlacklist);
+            blacklist = new ToolStripMenuItem();
+            CheckBlacklist();
+            this.icon.ContextMenuStrip.Items.Add(blacklist);
+            this.icon.ContextMenuStrip.Items.Add("-"); // spacer
+            this.icon.ContextMenuStrip.Items.Add("Add To Whitelist", null, this.ToggleBlacklist);
+            whitelist = new ToolStripMenuItem();
+            CheckWhitelist();
+            this.icon.ContextMenuStrip.Items.Add(whitelist);
+            this.icon.ContextMenuStrip.Items.Add("-"); // spacer
+            this.icon.ContextMenuStrip.Items.Add("Exit", null, this.MenuExit);
+        }
         void MenuExit(object sender, EventArgs e)
         {
             Environment.Exit(0);
@@ -45,6 +56,28 @@ namespace DAW_Presence
         {
             blacklist.Text = "Enable Blacklist";
             blacklist.Click += new EventHandler(this.ToggleBlacklist);
+        }
+
+        void ToggleWhitelist(object sender, EventArgs e)
+        {
+            whitelist.Text = "Disable Whitelist";
+        }
+
+        void CheckWhitelist()
+        {
+            whitelist.Text = "Enable Whitelist";
+            whitelist.Click += new EventHandler(this.ToggleWhitelist);
+        }
+
+        void ToggleRPC(object sender, EventArgs e)
+        {
+            rpc.Text = "Enable RPC";
+        }
+
+        void CheckRPCState()
+        {
+            rpc.Text = "Disable RPC";
+            rpc.Click += new EventHandler(this.ToggleRPC);
         }
     }
 }
